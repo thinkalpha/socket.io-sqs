e9759d5ef0847819ed8d2f5cb83d863190c0fd22
David Pfeffer <david@thinkalpha.io>
2020-10-05 16:54:36 -0400

    chattier cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,2 +1,8 @@
-for i in $(aws sqs list-queues --output text --no-paginate --query QueueUrls --queue-name-prefix "socketio-test-"); do aws sqs delete-queue --queue-url $i; done
-for i in $(aws sns list-topics  --query 'Topics[].[TopicArn]' --output=text | grep socketio-test-); do aws sns delete-topic --topic-arn $i; done
+for i in $(aws sqs list-queues --output text --no-paginate --query QueueUrls --queue-name-prefix "socketio-test-"); do
+    echo $i;
+    aws sqs delete-queue --queue-url $i;
+done
+for i in $(aws sns list-topics  --query 'Topics[].[TopicArn]' --output=text | grep socketio-test-); do
+    echo $i;
+    aws sns delete-topic --topic-arn $i;
+done
